cde8101f06d3da242bce94f8a21540b70a7ca823
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-07-06 15:48:43 +0900

    Tdd: 의존성 주입 됐는지 기대하는 함수

---
+++ ClickCounter/ClickCountView.js
@@ -0,0 +1,10 @@
+var App = App || {};
+
+App.ClickCounterView = (clickCounter, updateEl) => {
+  if (!clickCounter) throw Error("clickCounter");
+  return {
+    updateView() {
+      updateEl.innerHTML = clickCounter.getValue();
+    },
+  };
+};
--- ClickCounter/ClickCountView.spec.js
+++ ClickCounter/ClickCountView.spec.js
@@ -1,7 +1,28 @@
-describe('App.ClickCountView', ()=> {
-  describe('updateView()', ()=> {
-    it('ClickCounter의 getValue() 값을 출력한다', ()=> {
-      // todo 
+const { beforeEach } = require("node:test");
+
+describe("App.ClickCountView", () => {
+  let updateEl, clickCounter, view;
+  beforeEach(() => {
+    clickCounter = App.ClickCounter();
+    updateEl = document.createElement("span");
+    view = App.ClickCounterView(ClickCount, updateEl);
+  });
+
+  if (
+    ("주입 안됨있음",
+    () => {
+      const clickCounter = null;
+      const updateEl = document.createElement("span");
+
+      const acutal = () => App.ClickCounterView(clickCOunter, updateEl);
+      expect(acutal).toThrowError();
     })
-  })
-})
+  )
+    describe("updateView()", () => {
+      it("ClickCounter의 getValue() 값을 출력한다", () => {
+        const counterValue = clickCounter.getValue();
+        view.updateView();
+        expect(updateEl.innerHTML).toBe(counterValue.toString());
+      });
+    });
+});
